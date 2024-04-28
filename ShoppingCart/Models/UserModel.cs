5a125aa88f171ba2a6e2201f5412f322993034a1
GuyomTr <guillaume.terriere@epitech.eu>
2024-04-28 15:52:21 -0400

    Add User model to multiple projects

---
+++ ShoppingCart/Models/UserModel.cs
@@ -0,0 +1,17 @@
+
+
+namespace UserManagement.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string BirthDate { get; set; }
+        public string City { get; set; }
+        public int UserBalance { get; set; } = 1000;
+        // public List<CartItem> CartItems { get; set; } = new List<CartItem>();
+        // public List<Invoice> Invoices { get; set; }
+    }
+
+}
