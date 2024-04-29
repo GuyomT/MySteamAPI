574f19610f6a09fd71bfc15b7d952cd345ef5da0
GuyomTr <guillaume.terriere@epitech.eu>
2024-04-28 22:29:14 -0400

    Update package references in csproj files

---
+++ OrderManagement/Data/OrderContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Models;
+
+namespace OrderManagement.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
