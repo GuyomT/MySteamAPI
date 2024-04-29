f7e5b6e8e4dd86bc2a15d14b824d2c2f24173805
Midnighttechalt <119815124+Midnighttechalt@users.noreply.github.com>
2024-04-28 22:27:04 -0400

    Update UserModel.cs

--- OrderManagement/Models/UserModel.cs
+++ OrderManagement/Models/UserModel.cs
@@ -6,6 +6,5 @@ namespace OrderManagement.Models
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal TotalPrice { get; set; }
-        // Autres propriétés d'une commande
     }
 }
